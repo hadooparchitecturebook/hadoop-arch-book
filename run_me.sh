90a0ca0662cbc4940b774c31b33882f83eede66b
Mark Grover <grover.markgrover@gmail.com>
2014-06-02 23:55:38 -0700

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -15,6 +15,6 @@ python log_gen.py
 
 hive -f 01-create-raw-log-table.hql
 hive -f 02-create-parquet-log-table.hql
-#hive -f 03-populate-parquet-log-table.hql
-#hive -f 04-query-parquet-log-table.hql
+hive -f 03-populate-parquet-log-table.hql
+hive -f 04-query-parquet-log-table.hql
 
