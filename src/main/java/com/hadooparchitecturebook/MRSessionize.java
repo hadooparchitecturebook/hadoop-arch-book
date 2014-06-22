e0ae2464dff45cfb4028a343e4479e2515e10f35
Mark Grover <mgrover@cloudera.com>
2014-06-22 16:55:56 -0700

    Updating code based on Cloudera's CDH 4.5.0

--- src/main/java/com/hadooparchitecturebook/MRSessionize.java
+++ src/main/java/com/hadooparchitecturebook/MRSessionize.java
@@ -49,27 +49,24 @@ public class MRSessionize {
             result.set(sum);
             context.write(key, result);
         }
+    }
 
-        public static void main(String[] args) throws Exception {
-            Configuration conf = new Configuration();
-            String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-            if (otherArgs.length < 2) {
-                System.err.println("Usage: wordcount <in> [<in>...] <out>");
-                System.exit(2);
-            }
-            Job job = new Job(conf, "word count");
-            job.setJarByClass(MRSessionize.class);
-            job.setMapperClass(TokenizerMapper.class);
-            job.setCombinerClass(IntSumReducer.class);
-            job.setReducerClass(IntSumReducer.class);
-            job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(IntWritable.class);
-            for (int i = 0; i < otherArgs.length - 1; ++i) {
-                FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-            }
-            FileOutputFormat.setOutputPath(job,
-                    new Path(otherArgs[otherArgs.length - 1]));
-            System.exit(job.waitForCompletion(true) ? 0 : 1);
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
         }
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(MRSessionize.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
