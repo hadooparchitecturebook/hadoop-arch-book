bba54fe9e330741727b1042f49401d0516d5511e
Mark Grover <mgrover@cloudera.com>
2014-10-12 20:20:12 -0700

    Updating log_gen.py

--- ch06-clickstream/HiveSessionize/log_gen.py
+++ ch06-clickstream/HiveSessionize/log_gen.py
@@ -3,8 +3,9 @@ import time
 import datetime
 import random
 timestr = time.strftime("%Y%m%d-%H%M%S")
+num_clicks = 500
 
-otime = datetime.datetime(2014, 10, 10, 01, 30, 27)
+otime = datetime.datetime(2014, 10, 10, 0, 0, 0)
 timestr =  time.strftime("%Y%m%d-%H%M%S", otime.timetuple())
 
 f = open('access_log_'+timestr+'.log','w')
@@ -15,15 +16,23 @@ referers=["-","http://www.casualcyclist.com","http://bestcyclingreviews.com/top_
 resources=["/handle-bars","/stems","/wheelsets","/forks","/seatposts","/saddles","/shifters","/Store/cart.jsp?productID="]
 useragents=["Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36","Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1","Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25","Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201","Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0","Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))"]
 
+# Generate times during the day for which we click
+times=[]
+for i in xrange(0,num_clicks):
+       seconds = datetime.timedelta(seconds=random.randint(0,86399))
+       times.append(seconds)
 
-for i in xrange(0,500):
-        increment = datetime.timedelta(seconds=random.randint(30,3000))
-        otime += increment
+# Then, sort them. We do this because we want a random distribution of clicks during the day but since random() is
+# not sorted, we sort them afterwards.
+times.sort()
+
+# Now we randomly pick other columns, combine them one by one with our sorted list of times and write it to a file
+for i in xrange(0,num_clicks):
         uri = random.choice(resources)
         if uri.find("Store")>0:
                 uri += `random.randint(1000,1500)`
         ip = random.choice(ips)
         useragent = random.choice(useragents)
         referer = random.choice(referers)
-        f.write('%s - - [%s] "GET %s HTTP/1.0" 200 %s "%s" "%s"\n' % (random.choice(ips),otime.strftime('%d/%b/%Y:%H:%M:%S %z'),uri,random.randint(2000,5000),referer,useragent))
+        f.write('%s - - [%s] "GET %s HTTP/1.0" 200 %s "%s" "%s"\n' % (random.choice(ips),(otime+times[i]).strftime('%d/%b/%Y:%H:%M:%S %z'),uri,random.randint(2000,5000),referer,useragent))
 
