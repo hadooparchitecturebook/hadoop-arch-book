38eb9f15ad48cae1ad3c269207eea8268b3f6520
Mark Grover <mgrover@cloudera.com>
2015-02-01 10:36:51 -0800

    Adding code for parsing out genres

--- ch11-data-warehousing/oltp/reverse_etl.py
+++ ch11-data-warehousing/oltp/reverse_etl.py
@@ -4,7 +4,11 @@ import MySQLdb
 mydb = MySQLdb.connect(user="root", host="127.0.0.1", db="movielens")
 cur = mydb.cursor()
 
-cur.execute("SELECT * from users limit 5");
+cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='movielens' AND TABLE_NAME='movies'");
 
-for row in cur.fetchall() :
+resultset = cur.fetchall()
+
+genres = [elem[0].replace("genre_", "") for elem in resultset if "genre_" in elem[0]]
+
+for row in genres :
 	print row
