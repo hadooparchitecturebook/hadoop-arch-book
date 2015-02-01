26926377577bbe12ca7a507cc288d172176720f4
Mark Grover <mgrover@cloudera.com>
2015-02-01 09:11:59 -0800

    First untested pass of OLTP schema file

---
+++ ch11-data-warehousing/oltp/create_oltp_schema_mysql.sql
@@ -0,0 +1,47 @@
+CREATE SCHEMA IF NOT EXISTS oltp;
+use oltp;
+
+CREATE TABLE IF NOT EXISTS occupation (
+  id INT PRIMARY KEY NOT NULL,
+  occupation VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user(
+  id INT PRIMARY KEY NOT NULL,
+  gender CHAR(1) NOT NULL,
+  zipcode VARCHAR(5) NOT NULL,
+  age INT NOT NULL,
+  occupation_id INT NOT NULL,
+  last_modified DATETIME NOT NULL,
+  FOREIGN KEY(occupation_id) REFERENCES occupation(id)
+);
+
+CREATE TABLE IF NOT EXISTS genre(
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS movie(
+  id INT PRIMARY KEY NOT NULL,
+  title VARCHAR(128) NOT NULL,
+  release_date DATE NOT NULL,
+  video_release_date DATE NOT NULL,
+  imdb_url VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS movie_genre(
+  movie_id INT NOT NULL,
+  genre_id INT NOT NULL,
+  FOREIGN KEY(movie_id) REFERENCES movie(id),
+  FOREIGN KEY(genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE IF NOT EXISTS user_rating(
+  id INT NOT NULL AUTO_INCREMENT,
+  timestamp DATETIME NOT NULL,
+  user_id INT NOT NULL,
+  movie_id INT NOT NULL,
+  rating INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(movie_id) REFERENCES movie(id)
+);
