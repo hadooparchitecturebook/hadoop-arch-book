caea6a9d5f0aecc5adb612c82d97c08bdc48d225
Mark Grover <mgrover@cloudera.com>
2015-02-04 09:55:39 -0800

    More updates to reverse etl code, updating tables to not be auto increment unless absolutely necessary

--- ch11-data-warehousing/oltp/reverse_etl.py
+++ ch11-data-warehousing/oltp/reverse_etl.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
+import os
+import csv
 import MySQLdb
 
-mydb = MySQLdb.connect(user="root", host="127.0.0.1", db="movielens")
+mydb = MySQLdb.connect(user="root", host="127.0.0.1", db="oltp")
 cur = mydb.cursor()
 
-cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='movielens' AND TABLE_NAME='movies'");
 
-resultset = cur.fetchall()
+with open(os.path.join(os.path.expanduser('~'), 'ml-100k/u.genre'), 'r') as genre_csv:
+	genre_file = csv.reader(genre_csv, delimiter='|')
+	for row in genre_file:
+		if len(row) > 0:
+			cur.execute("INSERT INTO genre(id, name) VALUES (%s, %s)", row[0], row[1])
 
-genres = [elem[0].replace("genre_", "") for elem in resultset if "genre_" in elem[0]]
+occupation_id = 0
+with open(os.path.join(os.path.expanduser('~'), 'ml-100k/u.occupation'), 'r') as occupation_file:
+	for occupation in [element.strip() for element in occupation_file]:
+		cur.execute("INSERT INTO occupation(id, name) VALUES (%s, %s)", occupation_id++, occupation)
 
-for row in genres :
-	print row
+with open(os.path.join(os.path.expanduser('~'), 'ml-100k/u.user'), 'r') as user_csv:
+	user_file = csv.reader(user_csv, delimiter='|')
+	for row in user_file:
+		if len(row) > 0:
+			cur.execute("INSERT INTO genre(id, name) VALUES (%s, %s)", row[0], row[1])
+
+
+
+#cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='movielens' AND TABLE_NAME='movies'");
+
+#resultset = cur.fetchall()
