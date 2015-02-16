818c457b24ddd27e222d17fd9a47a6ffdedf0e7f
Mark Grover <mgrover@cloudera.com>
2015-02-16 06:33:23 -0800

    Adding info about setup on macox as a comment in the setup script

--- ch11-data-warehousing/oltp/00_setup.sh
+++ ch11-data-warehousing/oltp/00_setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sudo yum install MySQL-python
+# For Mac OSX, I followed http://stackoverflow.com/questions/1448429/how-to-install-mysqldb-python-data-access-library-to-mysql-on-mac-os-x
+# I also had to source out ARCHFLAGS environment variable when calling build and install commands, so I did something like:
+# sudo ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future python setup.py install
