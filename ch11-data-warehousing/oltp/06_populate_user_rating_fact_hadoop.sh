343dea72c4b0596769a58ce97933fbbe9723474a
Mark Grover <mgrover@cloudera.com>
2015-02-27 08:06:52 -0800

    Working on testing the incremental import command

--- ch11-data-warehousing/oltp/06_populate_user_rating_fact_hadoop.sh
+++ ch11-data-warehousing/oltp/06_populate_user_rating_fact_hadoop.sh
@@ -9,8 +9,19 @@ sqoop job --delete user_rating_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_
 sqoop job --create user_rating_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop \
 -- import --connect jdbc:mysql://mgrover-haa-2.vpc.cloudera.com:3306/oltp --username root \
 --table user_rating -m 8 --incremental append --check-column timestamp \
---as-parquetfile --hive-import --warehouse-dir /etl/movielens/user_rating_fact
+--as-parquetfile --hive-import --warehouse-dir /etl/movielens/user_rating_fact --hive-table user_rating_fact
 
-# Need to create a hive table for user_rating_fact here!!
+# The above command fails. We can either get rid of all hive things from the term or have to explicitly define
+# HIVE_HOME and export it before we run this. Otherwise it fails with a misleading error message org.kitesdk.data.DatasetNotFoundException: Unknown dataset URI: hive?dataset=user_rating_fact
+
+#Create user_rating_fact table in hive
+hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS user_rating_fact(id INT, timestamp TIMESTAMP,
+ user_id INT, rating INT, movie_id INT)
+ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
+STORED AS
+INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
+OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat'
+LOCATION '/etl/movielens/user_rating_fact'"
 
 sqoop job -exec user_rating_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop
+
