8ed4a5c0c95c6a441a3ac1bd3280b71349e26eb7
Mark Grover <mgrover@cloudera.com>
2015-02-28 15:23:40 -0800

    Adding some more comments

--- ch11-data-warehousing/oltp/06_populate_user_rating_fact_hadoop.sh
+++ ch11-data-warehousing/oltp/06_populate_user_rating_fact_hadoop.sh
@@ -6,13 +6,12 @@ sudo -u hdfs hadoop fs -chown -R $USER: /etl/movielens/user_rating_fact
 
 sqoop job --delete user_rating_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop || :
 
+# Need to explictly export HIVE_HOME before this command if Hive is not present under /usr/lib/hive
+# No need to do this if you are using Apache Sqoop 1.4.6 or later
 sqoop job --create user_rating_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop \
 -- import --connect jdbc:mysql://mgrover-haa-2.vpc.cloudera.com:3306/oltp --username root \
 --table user_rating -m 8 --incremental append --check-column timestamp \
 --as-parquetfile --hive-import --warehouse-dir /etl/movielens/user_rating_fact --hive-table user_rating_fact
-
-# The above command fails. We can either get rid of all hive things from the term or have to explicitly define
-# HIVE_HOME and export it before we run this. Otherwise it fails with a misleading error message org.kitesdk.data.DatasetNotFoundException: Unknown dataset URI: hive?dataset=user_rating_fact
 
 #Create user_rating_fact table in hive
 hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS user_rating_fact(id INT, timestamp TIMESTAMP,
