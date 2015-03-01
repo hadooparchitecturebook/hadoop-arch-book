d104a8c29926d75b8fdc08faa15d54c0ec6f2930
Mark Grover <mgrover@cloudera.com>
2015-03-01 10:18:44 -0800

    Trying yet another command

--- ch11-data-warehousing/oltp/10_export.sh
+++ ch11-data-warehousing/oltp/10_export.sh
@@ -4,4 +4,6 @@
 # CREATE TABLE avg_movie_rating(movie_id INT, rating DOUBLE);
 
 ##  THIS COMMAND DOESN'T WORK. NEED TO LOOK MORE INTO IT!
-sqoop export --connect jdbc:mysql://mgrover-haa-4.vpc.cloudera.com:3306/movie_dwh --username root --table avg_movie_rating --export-dir /user/hive/warehouse/avg_movie_rating -m 16 --update-key movie_id
+sqoop export --connect jdbc:mysql://mgrover-haa-2.vpc.cloudera.com:3306/movie_dwh --username root --table avg_movie_rating --export-dir /user/hive/warehouse/avg_movie_rating -m 16 --update-key movie_id
+
+sqoop export --connect jdbc:mysql://mgrover-haa-2.vpc.cloudera.com:3306/movie_dwh --username root --table avg_movie_rating --export-dir /data/movielens/aggregated_ratings -m 16 --update-key movie_id
