a1dc120763084d75f3ef07ca09772877b22a5dd8
Mark Grover <mgrover@cloudera.com>
2015-03-01 10:23:12 -0800

    Adding an alternative way to populate the avro table in Hive corresponding to the second export statement but it still doesn't help

--- ch11-data-warehousing/oltp/09_aggregations.sh
+++ ch11-data-warehousing/oltp/09_aggregations.sh
@@ -20,3 +20,18 @@ FROM
   user_rating_part
 GROUP BY
   movie_id"
+
+hive -e "
+CREATE EXTERNAL TABLE avg_movie_rating2(movie_id INT, rating DOUBLE)
+STORED AS AVRO
+LOCATION '/data/movielens/aggregated_ratings'"
+
+hive -e "
+INSERT INTO avg_movie_rating2
+SELECT
+  movie_id,
+  ROUND(avg(rating), 1) as rating
+FROM
+  user_rating_part
+GROUP BY
+  movie_id"
