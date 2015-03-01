acb01a8e0d0c6e7a83a0a127976161a7da976d9c
Mark Grover <mgrover@cloudera.com>
2015-03-01 00:08:01 -0800

    Fixing up the processing file. All should be good now

--- ch11-data-warehousing/oltp/07_data_processing.sh
+++ ch11-data-warehousing/oltp/07_data_processing.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-sudo -u hdfs hadoop fs -mkdir -p  /etl/movielens/user_rating_part
-sudo -u hdfs hadoop fs -chown -R $USER: /etl/movielens/user_rating_part
-hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS
-user_rating_part(id INT, timestamp BIGINT, user_id INT, movie_id INT, rating INT)
-PARTITIONED BY (year INT, month INT, day INT)
-ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
-STORED AS
-INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
-OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat'
-LOCATION '/etl/movielens/user_rating_part'"
-
 sudo -u hdfs hadoop fs -mkdir -p  /etl/movielens/user_rating
 sudo -u hdfs hadoop fs -chown -R $USER: /etl/movielens/user_rating
 # user_rating that compacts user_rating_fact to a single rating per user per movie
 # No need for id's here, since multiple user_rating_fact id's would potentially get squashed into 1
 # in this table, anyways.
 hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS
-user_rating(user_id INT, movie_id INT, rating INT, last_modified BIGINT)
+user_rating(user_id INT, movie_id INT, rating INT, last_modified TIMESTAMP)
 ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
 STORED AS
 INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
@@ -28,17 +17,30 @@ INSERT INTO TABLE user_rating
 SELECT
   user_id,
   movie_id,
-  UNIX_TIMESTAMP(SPLIT(MAX(CONCAT(FROM_UNIXTIME(timestamp), ',', rating)), ',')[0]) AS last_modified,
-  SPLIT(MAX(CONCAT(FROM_UNIXTIME(timestamp), ',', rating)), ',')[1] AS rating
+  SPLIT(MAX(CONCAT(FROM_UNIXTIME(INT(timestamp/1000)), ',', rating)), ',')[1] AS rating,
+  SPLIT(MAX(CONCAT(FROM_UNIXTIME(INT(timestamp/1000)), ',', rating)), ',')[0] AS last_modified
 FROM
   user_rating_fact
 GROUP BY
   user_id,
   movie_id"
 
+
+sudo -u hdfs hadoop fs -mkdir -p  /etl/movielens/user_rating_part
+sudo -u hdfs hadoop fs -chown -R $USER: /etl/movielens/user_rating_part
+hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS
+user_rating_part(user_id INT, movie_id INT, rating INT, last_modified TIMESTAMP)
+PARTITIONED BY (year INT, month INT, day INT)
+ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
+STORED AS
+INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
+OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat'
+LOCATION '/etl/movielens/user_rating_part'"
+
 hive -e "
 SET hive.exec.dynamic.partition.mode=nonstrict;
-SET  hive.exec.dynamic.partition=true;
+SET hive.exec.dynamic.partition=true;
+SET hive.exec.max.dynamic.partitions.pernode=1000;
 INSERT INTO TABLE user_rating_part PARTITION (year, month, day)
 SELECT
   *,
