c5413868e4c87cf6884232e08c0525e7d8c8a12b
Mark Grover <mgrover@cloudera.com>
2015-03-01 01:06:01 -0800

    Writing a wip note

--- ch11-data-warehousing/oltp/10_export.sh
+++ ch11-data-warehousing/oltp/10_export.sh
@@ -2,4 +2,6 @@
 # The destination table in DWH has to exist before running this command along with appropriate write permissions to the user running the sqoop job
 # Something like this should do:
 # CREATE TABLE avg_movie_rating(movie_id INT, rating DOUBLE);
+
+##  THIS COMMAND DOESN'T WORK. NEED TO LOOK MORE INTO IT!
 sqoop export --connect jdbc:mysql://mgrover-haa-4.vpc.cloudera.com:3306/movie_dwh --username root --table avg_movie_rating --export-dir /user/hive/warehouse/avg_movie_rating -m 16 --update-key movie_id
