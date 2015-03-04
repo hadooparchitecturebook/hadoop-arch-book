0ec0e4b2f3f4a431cc34ceebe52d1a5e9417474f
Mark Grover <mgrover@cloudera.com>
2015-03-03 21:44:31 -0800

    Something that works!

--- ch11-data-warehousing/oltp/08_users.sh
+++ ch11-data-warehousing/oltp/08_users.sh
@@ -7,24 +7,20 @@ sudo -u hdfs hadoop fs -rm -r /etl/movielens/user_upserts || :
 sudo -u hdfs hadoop fs -mkdir -p /etl/movielens/user_upserts
 sudo -u hdfs hadoop fs -chown -R $USER: /etl/movielens/user_upserts
 
+sudo -u hdfs hadoop fs -rm -r /user/hive/warehouse/user_upserts || :
+
 sudo -u hdfs hadoop fs -mkdir -p /data/movielens/user
 sudo -u hdfs hadoop fs -chown -R $USER: /data/movielens/user
 
 hive -e \
-"CREATE EXTERNAL TABLE IF NOT EXISTS user_upserts(id INT, age INT, occupation STRING, zipcode STRING, last_modified TIMESTAMP)
-ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
-STORED AS
-INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
-OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat'
+"CREATE EXTERNAL TABLE IF NOT EXISTS user_upserts(id INT, age INT, occupation STRING, zipcode STRING, last_modified BIGINT)
+STORED AS AVRO
 LOCATION '/etl/movielens/user_upserts'"
 
 hive -e \
-"CREATE EXTERNAL TABLE IF NOT EXISTS user_history(id INT, age INT, occupation STRING, zipcode STRING, last_modified TIMESTAMP)
+"CREATE EXTERNAL TABLE IF NOT EXISTS user_history(id INT, age INT, occupation STRING, zipcode STRING, last_modified BIGINT)
 PARTITIONED BY (year INT, month INT, day INT)
-ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
-STORED AS
-INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
-OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat'
+STORED AS AVRO
 LOCATION '/etl/movielens/user_history'"
 
 hive -e \
@@ -37,15 +33,14 @@ LOCATION '/data/movielens/user'"
 
 sqoop job --delete user_upserts_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop
 
-# Need to explictly export HIVE_HOME before this command if Hive is not present under /usr/lib/hive
-# No need to do this if you are using Apache Sqoop 1.4.6 or later
+#Couldn't do Parquet because there are some bugs related to incremental import and Parquet integration
 sqoop job --create user_upserts_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop \
 -- import --connect jdbc:mysql://mgrover-haa-2.vpc.cloudera.com:3306/oltp --username root \
--m 8 --incremental append --check-column last_modified --split-by last_modified --as-parquetfile \
+-m 8 --incremental append --check-column last_modified --split-by last_modified --as-avrodatafile \
 --query 'SELECT user.id, user.age, user.gender, 
 occupation.occupation, zipcode, last_modified FROM user JOIN occupation 
 ON (user.occupation_id = occupation.id) WHERE $CONDITIONS' \
---hive-import --hive-table user_upserts --target-dir /etl/movielens/user_upserts
+--target-dir /etl/movielens/user_upserts
 
 sqoop job -exec user_upserts_import --meta-connect jdbc:hsqldb:hsql://${SQOOP_METASTORE_HOST}:16000/sqoop
 
@@ -62,7 +57,7 @@ INSERT OVERWRITE TABLE user
     user_upserts.id IS NULL
   UNION ALL
   SELECT
-    *
+    id, age, occupation, zipcode, TIMESTAMP(last_modified)
   FROM
     user_upserts"
 
@@ -70,7 +65,7 @@ YEAR=$(date "+%y")
 MONTH=$(date "+%M")
 DAY=$(date "+%d")
 
-sudo -u hdfs hadoop fs -mkdir -p /etl/movielens/user_history/year=${YEAR}/month=${MONTH}/day={DAY}
+sudo -u hdfs hadoop fs -mkdir -p /etl/movielens/user_history/year=${YEAR}/month=${MONTH}/day=${DAY}
 sudo -u hdfs hadoop fs -mv /etl/movielens/user_upserts/* /etl/movielens/user_history/year=${YEAR}/month=${MONTH}/day=${DAY}
 
-hive -e "ALTER TABLE user_history ADD PARTITION (year={env:YEAR}, month={env:MONTH}, day={env:DAY})"
+hive -e "ALTER TABLE user_history ADD PARTITION (year=${YEAR}, month=${MONTH}, day=${DAY})"
