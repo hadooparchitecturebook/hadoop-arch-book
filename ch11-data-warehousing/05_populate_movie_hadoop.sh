21824f12da841983eebc57cd2c626f84d465f212
Mark Grover <grover.markgrover@gmail.com>
2015-03-15 22:33:30 -0700

    Update 05_populate_movie_hadoop.sh

--- ch11-data-warehousing/05_populate_movie_hadoop.sh
+++ ch11-data-warehousing/05_populate_movie_hadoop.sh
@@ -4,7 +4,8 @@ AVRO_HOME=/opt/cloudera/parcels/CDH/lib/avro
 sudo -u hdfs hadoop fs -mkdir -p /metadata/movielens/movie
 sudo -u hdfs hadoop fs -chown -R ${USER}: /metadata/movielens/movie
 
-# Clean up the destination schema file, if it exists. The last ||: ensures that we carry on the script even 
+# Clean up the destination schema file, if it exists. 
+# The last ||: ensures that we carry on the script even 
 # when the destination doesn't exist.
 sudo -u hdfs hadoop fs -rm /metadata/movielens/movie/movie.avsc || :
 hadoop jar ${AVRO_HOME}/avro-tools.jar getschema /data/movielens/movie/part-m-00000.avro | \
@@ -14,5 +15,5 @@ hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS movie
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe'
 STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'
-location '/data/movielens/movie'
+LOCATION '/data/movielens/movie'
 TBLPROPERTIES ('avro.schema.url'='/metadata/movielens/movie/movie.avsc')"
