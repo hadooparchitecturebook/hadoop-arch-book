6464a801deac0a11e21f45c531d19a6f7efce8ef
Mark Grover <grover.markgrover@gmail.com>
2015-03-15 22:31:11 -0700

    Update 04_sqoop_import.sh

--- ch11-data-warehousing/04_sqoop_import.sh
+++ ch11-data-warehousing/04_sqoop_import.sh
@@ -11,10 +11,15 @@ sudo -u hdfs hadoop fs -chown -R ${USER}: /user/${USER}
 
 # All nodes need to have access to the source database
 
-# Cleanup if necessary. Script should continue even if this next rm command fails, hence the last || :
+# Cleanup if necessary. Script should continue even if this next rm command fails.
+# Hence the last || :
 sudo -u hdfs hadoop fs -rm -r /data/movielens/movie || :
 
 sqoop import --connect jdbc:mysql://mgrover-haa-2.vpc.cloudera.com:3306/oltp --username root --query \
-'SELECT movie.*, group_concat(genre.name) FROM movie JOIN movie_genre ON (movie.id = movie_genre.movie_id)
-JOIN genre ON (movie_genre.genre_id = genre.id) WHERE ${CONDITIONS} GROUP BY movie.id' \
+'SELECT movie.*, group_concat(genre.name)
+FROM movie 
+JOIN movie_genre ON (movie.id = movie_genre.movie_id)
+JOIN genre ON (movie_genre.genre_id = genre.id) 
+WHERE ${CONDITIONS}
+GROUP BY movie.id' \
 --split-by movie.id --as-avrodatafile --target-dir /data/movielens/movie
