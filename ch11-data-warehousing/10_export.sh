da494c8c58bdaee9c35eb4d6b0e8100a9917bcba
Mark Grover <mgrover@cloudera.com>
2015-05-12 06:18:57 -0700

    Updating language of a comment

--- ch11-data-warehousing/10_export.sh
+++ ch11-data-warehousing/10_export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# The destination table in DWH has to exist before running this command along 
-# with appropriate write permissions to the user running the sqoop job
+# The destination table in DWH has to exist before this command is run
+# along with appropriate write permissions to the user running the sqoop job
 # Something like this should do:
 # CREATE TABLE avg_movie_rating(movie_id INT, rating DOUBLE);
 
